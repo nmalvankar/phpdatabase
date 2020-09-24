bd1724ab292112eed3d44d91063dd1ba6aa38103
nmalvankar <nmalvankar@gmail.com>
2020-09-23 22:50:19 -0400

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -5,6 +5,7 @@ $dbuser = getenv("databaseuser");
 $dbpwd = getenv("databasepassword");
 $dbname = getenv("databasename");
 
+/*
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
@@ -13,4 +14,14 @@ if ($connection->connect_errno) {
     printf("Connected to the database");
 }
 $connection->close();
+*/
+try {
+  $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpwd);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+$conn=null;
 ?>
